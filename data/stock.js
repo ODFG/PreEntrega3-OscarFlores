d945a1df62935e586a62111fcbd6b29d3c3c6985
Oscar Flores <odfg90@gmail.com>
2023-04-18 00:54:14 -0600

    Agregado código

---
+++ data/stock.js
@@ -0,0 +1,68 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Buzo 1",
+        tipo: "buzo",
+        desc: "Un buzo elegante",
+        precio: 1200,
+        talle: "M",
+        imagen: 'public/images/buzo.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Remera 1",
+        tipo: "remera",
+        desc: "Una remera negra",
+        precio: 500,
+        talle: "S",
+        imagen: 'public/images/remera.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Camisa 1",
+        tipo: "camisa",
+        desc: "Una camisa casual",
+        precio: 900,
+        talle: "M",
+        imagen: 'public/images/camisa.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Pantalon 1",
+        tipo: "pantalon",
+        desc: "Una pantalon blanco",
+        precio: 1600,
+        talle: "S",
+        imagen: 'public/images/pantalon.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Buzo 2",
+        tipo: "buzo",
+        desc: "Un buzo azul",
+        precio: 1200,
+        talle: "L",
+        imagen: 'public/images/buzo.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Remera 2",
+        tipo: "remera",
+        desc: "Una remera rosa",
+        precio: 500,
+        talle: "L",
+        imagen: 'public/images/remera.jpg',
+        cantidad:1
+    }
+];
